bf34310ed142c2d77e56b9577a3d4a39abac08b2
AlexxSome <alex.soto.29.9.88@gmail.com>
2024-02-06 21:48:31 -0300

    add js and new look for IMC

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
-$(document).ready(function(){
-    let peso = 0;
-    let estatura = 0;
+$(document).ready(function() {
+    function calculateIMC(weight, height) {
+        return weight / (height * height);
+    }
 
+    function getIMCCategory(imc) {
+        if (imc < 18.5) {
+            return "Bajo peso";
+        } else if (imc >= 18.5 && imc < 25) {
+            return "Peso normal";
+        } else if (imc >= 25 && imc < 30) {
+            return "Sobrepeso";
+        } else {
+            return "Obesidad";
+        }
+    }
 
-    $('#btnCalcular').click(function(){
-        alert("Hola mundete");
+    function resetForm() {
+        $("#weight").val("");
+        $("#height").val("");
+        $("#inputForm").show();
+        $("#result").hide();
+    }
+
+    $("#calculateBtn").click(function() {
+        var weight = parseFloat($("#weight").val());
+        var height = parseFloat($("#height").val());
+
+        var imc = calculateIMC(weight, height);
+        var category = getIMCCategory(imc);
+
+        $("#imc").text(imc.toFixed(2));
+        $("#category").text(category);
+        $("#inputForm").hide();
+        $("#result").show();
     });
 
-})
+    $("#restartBtn").click(function() {
+        resetForm();
+    });
+
+    $("#exitBtn").click(function() {
+        resetForm();
+        $("#inputForm").hide();
+        $("#result").hide();
+    });
+});
